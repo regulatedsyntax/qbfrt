1007798edaffc2967850ca8279381856931cb68b
RegulatedSyntax <211179665+regulatedsyntax@users.noreply.github.com>
2025-05-11 21:13:30 +0100

    refactor: tracker_url tools

--- src/db/tracker_url.rs
+++ src/db/tracker_url.rs
@@ -1,10 +1,10 @@
 //! Tools for modifying torrent tracker URLs
 
+use crate::common::database::LibtorrentResumeData;
 use crate::common::fastresume::Fastresume;
 use crate::config::Config;
-use crate::db::db_structs::FetchedLibtorrentResumeData;
+use crate::db::query;
 use rusqlite::{named_params, Connection};
-use serde_rusqlite::from_rows;
 use std::error::Error;
 
 /// Tracker url information
@@ -21,11 +21,12 @@ pub struct TrackerUrl {
 /// ## Example
 /// ```rs
 /// use qbfrt::db::{tracker_url, TrackerUrl};
+/// let config = Config { verbose: true };
 /// let tracker_url = TrackerUrl {
 ///     old: String::from("http://"),
 ///     new: String::from("https://"),
 /// };
-/// change_tracker_url(&connection, tracker_url, false);
+/// change_tracker_url(&connection, tracker_url, config);
 /// ```
 ///
 /// ## Verbose output
@@ -41,23 +42,23 @@ pub fn change_tracker_url(
         tracker_url.old, tracker_url.new
     );
 
-    // First we fetch every torrent and iterate through it to see if it needs an update
-    let mut search_stmt = db.prepare(
-        "SELECT id, torrent_id, libtorrent_resume_data
-        FROM torrents",
+    let all_torrents = query::fetch_all_torrents::<LibtorrentResumeData>(
+        db,
+        "
+        SELECT id, torrent_id, libtorrent_resume_data 
+        FROM torrents
+        ",
     )?;
-    let all_torrents = from_rows::<FetchedLibtorrentResumeData>(search_stmt.query([])?);
 
-    let mut num_updated = 0;
-    for row in all_torrents {
-        let torrent = row?;
-        let bencoded_data = torrent.libtorrent_resume_data.as_slice();
-        let mut libtorrent_resume_data: Fastresume = serde_bencode::from_bytes(bencoded_data)?;
-
+    let mut num_torrents_updated = 0;
+    for torrent in all_torrents {
         let mut trigger_update = false;
+        let bencoded_resume_data = torrent.libtorrent_resume_data.as_slice();
+        let mut resume_data: Fastresume = serde_bencode::from_bytes(bencoded_resume_data)?;
+
         // Trackers are stored in a nested bencode list
         let mut updated_trackers: Vec<Vec<String>> = Vec::new();
-        libtorrent_resume_data.trackers.iter().for_each(|outer| {
+        resume_data.trackers.iter().for_each(|outer| {
             let mut tracker_list: Vec<String> = Vec::new();
 
             outer.iter().for_each(|tracker| {
@@ -72,34 +73,45 @@ pub fn change_tracker_url(
             updated_trackers.push(tracker_list);
         });
 
-        libtorrent_resume_data.trackers = updated_trackers;
+        resume_data.trackers = updated_trackers;
 
         if trigger_update {
             let mut update_stmt = db.prepare(
-                "UPDATE torrents
+                "
+                UPDATE torrents
                 SET libtorrent_resume_data = :lrd
                 WHERE id = :id
-                RETURNING torrent_id;",
+                RETURNING torrent_id;
+                ",
             )?;
-            update_stmt.query_row(named_params! {":lrd": serde_bencode::to_bytes(&libtorrent_resume_data)?, ":id": torrent.id}, |row| {
-                let updated_row_id = row.get::<usize, String>(0)?;
+            update_stmt.query_row(
+                named_params! {":lrd": serde_bencode::to_bytes(&resume_data)?, ":id": torrent.id},
+                |row| {
+                    let updated_row_id = row.get::<usize, String>(0)?;
 
-                if config.verbose {
-                    println!("Tracker url: updated tracker URLs for {}", updated_row_id);
-                    println!("{}: new tracker urls are {:?}", updated_row_id, libtorrent_resume_data.trackers);
-                }
+                    if config.verbose {
+                        println!("Tracker url: updated tracker URLs for {}", updated_row_id);
+                        println!(
+                            "{}: new tracker urls are {:?}",
+                            updated_row_id, resume_data.trackers
+                        );
+                    }
 
-                num_updated += 1;
+                    num_torrents_updated += 1;
 
-                Ok(())
-            })?;
+                    Ok(())
+                },
+            )?;
         }
     }
 
-    match num_updated {
+    match num_torrents_updated {
         0 => print!("Tracker url: no torrents were updated"),
         1 => println!("Tracker url: 1 torrent was updated"),
-        _ => println!("Tracker url: {} torrents were updated", num_updated),
+        _ => println!(
+            "Tracker url: {} torrents were updated",
+            num_torrents_updated
+        ),
     }
 
     Ok(())
